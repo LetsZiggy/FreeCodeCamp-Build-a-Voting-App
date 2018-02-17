0b0546d344afd7c00aff66a496a49d09858bfd6c
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2018-02-18 07:45:04 +0800

    Polls (Mostly done)

--- aurelia/src/resources/modules/home.js
+++ aurelia/src/resources/modules/home.js
@@ -6,39 +6,60 @@ export class Home {
   @bindable({ defaultBindingMode: bindingMode.twoWay }) state = state;
 
   constructor() {
-    this.charts = [];
+    this.charts = { created: [], participated: [], latest: [], most: [] };
   }
 
   attached() {
+    // if(this.state.login) {
+    //   this.state.user.created.forEach((v, i, a) => {
+    //     this.charts.push(drawChart(`created-canvas-${v.id}`, v.choices));
+    //   });
+
+    //   this.state.user.participated.forEach((v, i, a) => {
+    //     this.charts.push(drawChart(`participated-canvas-${v.id}`, v.choices));
+    //   });
+    // }
+
+    // this.state.polls.forEach((v, i, a) => {
+    //   if(document.getElementById(`latest-canvas-${v.id}`)) {
+    //     this.charts.push(drawChart(`latest-canvas-${v.id}`, v.choices));
+    //   }
+
+    //   if(document.getElementById(`most-canvas-${v.id}`)) {
+    //     this.charts.push(drawChart(`most-canvas-${v.id}`, v.choices));
+    //   }
+    // });
+
+    let canvas = [];
+
     if(this.state.login) {
-      this.state.user.created.forEach((v, i, a) => {
-        this.charts.push(drawChart(`created-canvas-${v.id}`, v.choices));
-      });
-
-      this.state.user.participated.forEach((v, i, a) => {
-        this.charts.push(drawChart(`participated-canvas-${v.id}`, v.choices));
-      });
+      canvas.push(['created', document.getElementById('created').getElementsByTagName('canvas')]);
+      canvas.push(['participated', document.getElementById('participated').getElementsByTagName('canvas')]);
     }
+    
+    canvas.push(['latest', document.getElementById('latest').getElementsByTagName('canvas')]);
+    canvas.push(['most', document.getElementById('most').getElementsByTagName('canvas')]);
 
-    this.state.polls.forEach((v, i, a) => {
-      if(document.getElementById(`latest-canvas-${v.id}`)) {
-        this.charts.push(drawChart(`latest-canvas-${v.id}`, v.choices));
-      }
-
-      if(document.getElementById(`most-canvas-${v.id}`)) {
-        this.charts.push(drawChart(`most-canvas-${v.id}`, v.choices));
-      }
-    });
+    generateChart(canvas, this.charts, this.state);
   }
 
   detached() {
-    this.charts.forEach((v, i, a) => {
-      v.destroy();
-    });
-    while(this.charts.length) {
-      this.charts.pop();
-    }
+    destroyChart(this.charts);
+    this.charts = null;
   }
+}
+
+function generateChart(canvas, charts, state) {
+  canvas.forEach((cv, ci, ca) => {
+    Array.from(cv[1]).forEach((v, i, a) => {
+      let id = v.getAttribute('id').split('-')[2];
+      let choice = null;
+      if(cv[0] === 'created') { choice = state.user.created.find(x => x.id === id); }
+      else if(cv[0] === 'participated') { choice = state.user.participated.find(x => x.id === id); }
+      else { choice = state.polls.find(x => x.id === id); }
+      charts[cv[0]].push(drawChart(v.getAttribute('id'), choice.choices));
+    });
+  });
 }
 
 function drawChart(id, choices) {
@@ -77,14 +98,15 @@ function drawChart(id, choices) {
             display: false,
             barPercentage: 1,
             categoryPercentage: 1,
-            barThickness: 5,
-            ticks: {
+            barThickness: 5
+,            ticks: {
               min: 0,
               max: max
             },
             stacked: true
           }]
         },
+        maintainAspectRatio: false,
         tooltips: {
           enabled: false
         },
@@ -95,4 +117,27 @@ function drawChart(id, choices) {
       }
     })
   );
+}
+
+function destroyChart(charts, type='object') {
+  if(type === 'object') {
+    Object.entries(charts).forEach(([ok, ov]) => {
+      ov.forEach((v, i, a) => {
+        v.destroy();
+      });
+
+      while(ov.length) {
+        ov.pop();
+      }
+    });
+  }
+  else {
+    charts.forEach((v, i, a) => {
+      v.destroy();
+    });
+
+    while(charts.length) {
+      charts.pop();
+    }
+  }
 }
--- aurelia/src/resources/modules/polls.js
+++ aurelia/src/resources/modules/polls.js
@@ -7,33 +7,79 @@ export class Polls {
 
   constructor() {
     this.pagination = { perPage: 10, latest: 0, most: 0 };
-    this.charts = [];
+    this.charts = { latest: [], most: [] };
   }
 
   attached() {
     // this.state.polls.forEach((v, i, a) => {
     //   if(document.getElementById(`latest-canvas-${v.id}`)) {
-    //     this.charts.push(drawChart(`latest-canvas-${v.id}`, v.choices));
+    //     this.charts.latest.push(drawChart(`latest-canvas-${v.id}`, v.choices));
     //   }
 
     //   if(document.getElementById(`most-canvas-${v.id}`)) {
-    //     this.charts.push(drawChart(`most-canvas-${v.id}`, v.choices));
+    //     this.charts.most.push(drawChart(`most-canvas-${v.id}`, v.choices));
     //   }
     // });
+    
+    let canvas = [
+      ['latest', document.getElementById('latest').getElementsByTagName('canvas')],
+      ['most', document.getElementById('most').getElementsByTagName('canvas')]
+    ];
+
+    generateChart(canvas, this.charts, this.state);
   }
 
   detached() {
-    this.charts.forEach((v, i, a) => {
-      v.destroy();
-    });
-    while(this.charts.length) {
-      this.charts.pop();
-    }
+    destroyChart(this.charts);
+    this.charts = null;
   }
 
   setPaginationAmount(amount) {
+    destroyChart(this.charts);
     this.pagination.perPage = Number(amount);
+
+    setTimeout(() => {
+      let canvas = [
+        ['latest', document.getElementById('latest').getElementsByTagName('canvas')],
+        ['most', document.getElementById('most').getElementsByTagName('canvas')]
+      ];
+
+      generateChart(canvas, this.charts, this.state);
+    }, 0);
   }
+
+  changePage(direction, poll) {
+    destroyChart(this.charts[poll], 'array');
+
+    if(direction === 'prev') {
+      this.pagination[poll] = (this.pagination[poll] - this.pagination.perPage) < 0
+                                ? 0
+                                : this.pagination[poll] - this.pagination.perPage;
+    }
+    else {
+      this.pagination[poll] = (this.pagination[poll] + this.pagination.perPage) > this.state.polls.length
+                                ? this.state.polls.length - this.pagination.perPage
+                                : this.pagination[poll] + this.pagination.perPage;
+    }
+
+    setTimeout(() => {
+      let canvas = [[poll, document.getElementById(poll).getElementsByTagName('canvas')]];
+      generateChart(canvas, this.charts, this.state);
+    }, 0);
+  }
+}
+
+function generateChart(canvas, charts, state) {
+  canvas.forEach((cv, ci, ca) => {
+    Array.from(cv[1]).forEach((v, i, a) => {
+      let id = v.getAttribute('id').split('-')[2];
+      let choice = null;
+      if(cv[0] === 'created') { choice = state.user.created.find(x => x.id === id); }
+      else if(cv[0] === 'participated') { choice = state.user.participated.find(x => x.id === id); }
+      else { choice = state.polls.find(x => x.id === id); }
+      charts[cv[0]].push(drawChart(v.getAttribute('id'), choice.choices));
+    });
+  });
 }
 
 function drawChart(id, choices) {
@@ -72,14 +118,15 @@ function drawChart(id, choices) {
             display: false,
             barPercentage: 1,
             categoryPercentage: 1,
-            barThickness: 5,
-            ticks: {
+            barThickness: 5
+,            ticks: {
               min: 0,
               max: max
             },
             stacked: true
           }]
         },
+        maintainAspectRatio: false,
         tooltips: {
           enabled: false
         },
@@ -90,4 +137,27 @@ function drawChart(id, choices) {
       }
     })
   );
+}
+
+function destroyChart(charts, type='object') {
+  if(type === 'object') {
+    Object.entries(charts).forEach(([ok, ov]) => {
+      ov.forEach((v, i, a) => {
+        v.destroy();
+      });
+
+      while(ov.length) {
+        ov.pop();
+      }
+    });
+  }
+  else {
+    charts.forEach((v, i, a) => {
+      v.destroy();
+    });
+
+    while(charts.length) {
+      charts.pop();
+    }
+  }
 }
