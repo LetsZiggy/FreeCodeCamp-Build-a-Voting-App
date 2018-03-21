284c164b91f5a93a3e2e0661034c817a58632712
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2018-03-21 21:28:35 +0800

    Aurelia.js Production build

--- aurelia/src/resources/services/api-interface.js
+++ aurelia/src/resources/services/api-interface.js
@@ -5,8 +5,8 @@ import {HttpClient} from 'aurelia-fetch-client';
 export class ApiInterface {
   constructor(HttpClient) {
     HttpClient.configure(config => {
-      config.withBaseUrl('http://localhost:3000/api')
-      // config.withBaseUrl('https://letsziggy-freecodecamp-build-a-voting-app.glitch.me/api')
+      // config.withBaseUrl('http://localhost:3000/api')
+      config.withBaseUrl('https://letsziggy-freecodecamp-build-a-voting-app.glitch.me/api')
             .withInterceptor({
               request(request) {
                 return request;
