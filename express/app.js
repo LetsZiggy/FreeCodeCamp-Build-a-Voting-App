54919d8ea44b2f9f71fd710302bc2370d089a738
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2018-03-22 21:47:10 +0800

    Update packages

--- express/app.js
+++ express/app.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const index = require('./routes/index');
 const api = require('./routes/api');
@@ -16,11 +15,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,9 +26,7 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
--- express/routes/index.js
+++ express/routes/index.js
@@ -5,6 +5,6 @@ const path = require('path');
 // Path to be changed on deployment
 router.get('/scripts/:filename', (req, res, next) => res.sendFile(path.join(__dirname, `../../aurelia/scripts/${req.params.filename}`)));
 
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index', { title: 'Express' }));
 
 module.exports = router;
